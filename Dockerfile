3b667c849d78d2916fd1241fe45e26de9741b1b1
imthaghost <imthaghost@protonmail.com>
2024-01-22 21:36:14 -0500

    deployments | switch to bubbletea UI

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM golang:alpine3.19
+
+# Enviornment variables
+ENV APP_NAME gitrepos
+ENV PORT 2222
+
+
+RUN apk update
+
+# Open system port
+EXPOSE ${PORT}
+
+# Working directory
+WORKDIR /go/src/${APP_NAME}
+
+COPY . /go/src/${APP_NAME}
+
+# Install dependecies from mod file
+RUN go mod download
+
+# Build application
+RUN go build -o ${APP_NAME} ./cmd/${APP_NAME}
+
+# Run application
+CMD ./${APP_NAME}
--- cmd/gitrepos/main.go
+++ cmd/gitrepos/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/gdamore/tcell/v2"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/gliderlabs/ssh"
-	"github.com/rivo/tview"
 )
 
 type Repo struct {
@@ -14,34 +17,90 @@ type Repo struct {
 	Stars int
 }
 
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+type model struct {
+	list list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
+}
 func main() {
+
+	items := []list.Item{
+		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
+		item{title: "Nutella", desc: "It's good on toast"},
+		item{title: "Bitter melon", desc: "It cools you down"},
+		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
+		item{title: "Eight hours of sleep", desc: "I had this once"},
+		item{title: "Cats", desc: "Usually"},
+		item{title: "Plantasia, the album", desc: "My plants love it too"},
+		item{title: "Pour over coffee", desc: "It takes forever to make though"},
+		item{title: "VR", desc: "Virtual reality...what is there to say?"},
+		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
+		item{title: "Linux", desc: "Pretty much the best OS"},
+		item{title: "Business school", desc: "Just kidding"},
+		item{title: "Pottery", desc: "Wet clay is a great feeling"},
+		item{title: "Shampoo", desc: "Nothing like clean hair"},
+		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
+		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
+		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
+		item{title: "Stickers", desc: "The thicker the vinyl the better"},
+		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
+		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
+		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
+		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
+		item{title: "Terrycloth", desc: "In other words, towel fabric"},
+	}
+
+	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "My Fave Things"
+
 	ssh.Handle(func(s ssh.Session) {
-		// Initialize TUI application
-		app := tview.NewApplication()
+		p := tea.NewProgram(m, tea.WithAltScreen())
 
-		// Fetch GitHub repositories (pseudo-function)
-		repos := fetchGitHubRepos() // Implement this function
-
-		// Create a list to display repositories
-		list := tview.NewList().ShowSecondaryText(false)
-
-		// Add repositories to the list
-		for _, repo := range repos {
-			list.AddItem(repo.Name, "", 0, nil)
+		if _, err := p.Run(); err != nil {
+			fmt.Println("Error running program:", err)
+			os.Exit(1)
+		}
+		if err := p.Start(); err != nil {
+			fmt.Printf("Alas, there's been an error: %v", err)
+			os.Exit(1)
 		}
 
-		// Add color and attributes
-		list.SetSelectedBackgroundColor(tcell.ColorGreen).
-			SetSelectedTextColor(tcell.ColorBlack)
-
-		// Set up the application
-		if err := app.SetRoot(list, true).Run(); err != nil {
-			panic(err)
-		}
+		log.Println("starting ssh server on port 2222...")
+		log.Fatal(ssh.ListenAndServe(":2222", nil))
 	})
 
-	log.Println("starting ssh server on port 2222...")
-	log.Fatal(ssh.ListenAndServe(":2222", nil))
 }
 
 // fetchGitHubRepos returns a list of GitHub repositories.
